0efacaea0040b3818afb9eff2e8c9cab7fe93eb2
arm <adrianmoravec@Adrians-MacBook-Pro.local>
2019-04-17 14:38:07 -0500

    bamazonCustomer should be working

---
+++ bamazonCustomer.js
@@ -0,0 +1,110 @@
+var inquirer = require('inquirer');
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazon",
+
+    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
+});
+
+listItems();
+
+function listItems() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // console.log(res);
+
+        for (i = 0; i < res.length; i++) {
+            console.log("Item #" + res[i].item_id + ": Product: " +
+                res[i].product_name + "- Price: $" + res[i].price);
+        };
+        askQuestion();
+    });
+};
+
+function askQuestion() {
+
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the Item # of the product you'd like to buy?",
+                name: "idNumber"
+            },
+            {
+                type: "input",
+                message: "How many would you like to buy?",
+                name: "quantity"
+            },
+        ])
+        .then(answers => {
+            // console.log(answers);
+            checkQuantity(answers.idNumber, answers.quantity);
+        });
+}
+
+function checkQuantity(id, userBoughtNum) {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // console.log(res);
+
+        var arrayId = id - 1;
+        var stockNum = res[arrayId].stock_quantity;
+        var userCost = res[arrayId].price * userBoughtNum;
+        var itemName = res[arrayId].product_name;
+
+        if (stockNum >= userBoughtNum) {
+            console.log("you can buy it!");
+
+            var newStockNum = stockNum - userBoughtNum;
+            buyProduct(id, newStockNum, userCost, userBoughtNum, itemName);
+        } else {
+            console.log("Insufficient quantity, sorry.");
+            buyAgain();
+        }
+    });
+
+};
+
+function buyProduct(id, newStock, userCost, userBoughtNum, itemName) {
+    connection.query("UPDATE products SET stock_quantity = " + newStock +
+        " WHERE item_id =" + id, function (err, res) {
+            if (err) throw err;
+            // console.log(res);
+            console.log("You have purchased " + userBoughtNum + " " +
+                itemName + " for the price of $" + userCost + ". Thank you!");
+            buyAgain();
+        });
+};
+
+function buyAgain() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Would you like to buy something else?",
+                choices: ["Yes", "No"],
+                name: "buyAgain"
+            },
+        ])
+        .then(answers => {
+            // console.log(answers);
+            if (answers.buyAgain === "Yes") {
+                listItems();
+            } else {
+                console.log("Thanks for shopping. Come again!");
+                connection.end();
+            }
+        });
+}
---
+++ seeds.sql
@@ -0,0 +1,33 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Flour", "Baking", "2.99", "100");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sugar", "Baking", "2.50", "100");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Whole Milk (gallon)", "Dairy", "2.64", "200");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Cheddar Cheese (grated)", "Dairy", "2.20", "80");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Root Beer (2 liter)", "Beverage", "1.60", "75");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Grape Juice", "Beverage", "3.34", "50");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pork Shoulder", "Meat", "1.99", "40");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Chicken Breast", "Meat", "2.44", "70");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Red Cabbage", "Produce", "1.79", "38");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Granny Smith Apple", "Product", "1.88", "200");
+
+select * from products
